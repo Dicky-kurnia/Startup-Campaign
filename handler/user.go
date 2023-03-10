a212fb4dc5bb67daf557a05d23e220647030adc4
Dicky-kurnia <dickykurnia190796@gmail.com>
2023-03-10 11:21:25 +0700

    feat: Upload Avatar Endpoint

--- handler/user.go
+++ handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"StartUp-Campaign/helper"
 	"StartUp-Campaign/user"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -92,4 +93,35 @@ func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
 	}
 	response := helper.APIresponse(metaMessage, http.StatusOK, "error", data)
 	c.JSON(http.StatusOK, response)
+}
+
+func (h *userHandler) UploadAvatar(c *gin.Context) {
+	file, err := c.FormFile("avatar")
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIresponse("Failed to Upload Avatar Image1", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	userID := 1
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIresponse("Failed to Upload Avatar Image2", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	_, err = h.userService.SaveAvatar(userID, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIresponse("Failed to Upload Avatar Image3", http.StatusBadRequest, "error", data)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	data := gin.H{"is_uploaded": true}
+	response := helper.APIresponse("Avatar successfully Uploaded", http.StatusOK, "succsess", data)
+	c.JSON(http.StatusOK, response)
+
 }
--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
 
 	err = router.Run(":8080")
 	if err != nil {
